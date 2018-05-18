b00c74abcebb3455a20707e38ebda9cac3e818f5
femio23 <chiscazo.paco@gmail.com>
2018-05-18 23:34:20 +0200

    Final

---
+++ src/boletin28/Boletin28.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin28;
+
+/**
+ *
+ * @author femio23
+ */
+public class Boletin28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alumno a1=new Alumno("Juan",7,"Avellaneda",25);
+        a1.amosar();
+        a1.cambiarnota(6);
+        a1.cambiorua("A Estrada");
+        a1.amosar();
+        
+    }
+    
+}
